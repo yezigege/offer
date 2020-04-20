a1c6d288d92bbea4a082ea307a8493db3f7144e7
叶中强 <18839136833@163.com>
2020-04-20 14:25:15 +0800

    更新散迭的文件

--- 排序/快速排序.py
+++ 排序/快速排序.py
@@ -1,17 +1,15 @@
-"""快速排序"""
-
-
 def quicksort(arr):
     if len(arr) < 2:
         return arr
     else:
         p = arr[0]
-        less = [x for x in arr[1:] if x < p]
-        big = [x for x in arr[1:] if x > p]
+        less = [i for i in arr[1:] if i < p]
+        big = [i for i in arr[1:] if i > p]
         return quicksort(less) + [p] + quicksort(big)
 
 
 if __name__ == '__main__':
-    list1 = [4, 2, 9, 7, 8, 3, 1, 0, 2, 5]
+    list1 = [7, 3, 23, 6, 9, 10]
     res = quicksort(list1)
     print(res)
+
